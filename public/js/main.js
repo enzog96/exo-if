23101336eac4f66de45284e820d0ee938d475146
enzo grillo <enzogrillo96@gmail.com>
2022-05-06 14:53:18 +0200

    first

---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+// console.log(1 == "1")
+
+// console.log(1 === "1")
+
+// let ask = prompt("Quel est votre nom?");
+// if (ask.length === 5) {
+//     alert("bon")
+    
+// } else {
+//     alert("nono")
+// }
+
+
+// let ask = prompt("1 + 1 = ?");
+// if (ask == 2) {
+//     alert("bon")
+    
+// } else {
+//     alert("nono")
+// }
+
+
+// let answer = 30
+// let ask = Number(prompt("6 * 5 = ?"));
+// if (ask === 30) {
+//     alert("la réponse était bien " + answer)
+    
+// } else {
+//     alert( `C'est dommage, tu n'étais qu'a ${answer - ask} unité de la bonne réponse `)
+// }
+
+
+
+// let array = [];
+
+// while (array.length < 3) {
+//     let question = prompt("gimme an array")
+//     if (array.length < 3){
+//         array.push(question) 
+//     } else {
+//         alert(array)
+//     }
+    
+// }
+    
+
+
